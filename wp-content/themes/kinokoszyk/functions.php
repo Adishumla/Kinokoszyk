5c7416c6de8d0cf52be7301c43497add40812483
JosefForkman <strand.vatten@outlook.com>
2023-03-26 10:27:36 +0100

    add tailwindcss

--- wp-content/themes/kinokoszyk/functions.php
+++ wp-content/themes/kinokoszyk/functions.php
@@ -1,22 +1,23 @@
 <?php
-// Load Tailwind from CDN
-// https://tailwindcss.com/docs/installation/play-cdn
+# Load Tailwind from CDN
+# https://tailwindcss.com/docs/installation/play-cdn
 function load_tailwind()
 {
-  wp_enqueue_script('tailwind', 'https://cdn.tailwindcss.com', array(), null, false);
-  // Load tailwind.config.js after tailwind has been loaded
+  # wp_enqueue_script('tailwind', 'https://cdn.tailwindcss.com', array(), null, false);
+  wp_enqueue_style("tailwind", get_template_directory_uri() . '/css/output.css', array(), true);
+  # Load tailwind.config.js after tailwind has been loaded
   wp_enqueue_script('tailwind-config', get_template_directory_uri() . '/tailwind.config.js', array('tailwind'), null, false);
 }
 add_action('wp_enqueue_scripts', 'load_tailwind');
 
-// Register theme support
+# Register theme support
 add_action('after_setup_theme', function () {
   add_theme_support('title-tag');
   add_theme_support('menus');
 });
 
-// Register theme menu(s)
-// https://developer.wordpress.org/themes/functionality/navigation-menus/
+# Register theme menu(s)
+# https://developer.wordpress.org/themes/functionality/navigation-menus/
 function register_theme_menus()
 {
   register_nav_menus(array(
@@ -26,7 +27,7 @@ function register_theme_menus()
 }
 add_action('init', 'register_theme_menus');
 
-// Print array or object in a pre tag
+# Print array or object in a pre tag
 function print_a($data)
 {
 ?>
