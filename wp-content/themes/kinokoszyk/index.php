c35e51d0f851ec157fae18ba6f319f6ecbbd4e82
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-03-27 15:56:38 +0200

    Update .gitignore

--- wp-content/themes/kinokoszyk/index.php
+++ wp-content/themes/kinokoszyk/index.php
@@ -1,4 +1,5 @@
-
+<link rel="stylesheet" href="css/output.css">
+<link rel="stylesheet" href="css/tailwind.css">
 <?php get_header(); ?>
 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
   Button
