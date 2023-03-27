ba4aeef579eeabc41eaaf359245ae87e6804eaaf
Josef Forkman <strand.vatten@outlook.com>
2023-03-27 09:09:52 +0200

    add themes icon

---
+++ wp-content/themes/kinokoszyk/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php wp_head(); ?>
+        <link rel='stylesheet' href='css/style.css'>
+        <title></title>
+    </head>
+    <body>
+        <?php wp_body_open(); ?>
