bc8e9ddc409dd10519da71ae5e180ac167cb7dbe
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-03-27 15:30:59 +0200

    Update .gitignore

--- wp-content/themes/kinokoszyk/index.php
+++ wp-content/themes/kinokoszyk/index.php
@@ -1,7 +1,6 @@
+
 <?php get_header(); ?>
 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
   Button
 </button>
-
-<h1>TESTIMESTTEST</h1>
 <?php get_footer(); ?>
