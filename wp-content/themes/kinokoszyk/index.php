4ce12e97f04e3ae6798ac856343fe59c93554a96
Josef Forkman <strand.vatten@outlook.com>
2023-03-30 11:01:28 +0200

    add image

--- wp-content/themes/kinokoszyk/index.php
+++ wp-content/themes/kinokoszyk/index.php
@@ -1,7 +1,4 @@
 <link rel="stylesheet" href="css/output.css">
 <link rel="stylesheet" href="css/tailwind.css">
 <?php get_header(); ?>
-<button class="btn-blue">
-  Button
-</button>
 <?php get_footer(); ?>
