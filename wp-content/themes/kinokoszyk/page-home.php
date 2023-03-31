28c987926fd162c6a55601c621414ec2ea190868
lovbackan <simon@lovbacka.se>
2023-03-31 15:01:09 +0200

    update on home

--- wp-content/themes/kinokoszyk/page-home.php
+++ wp-content/themes/kinokoszyk/page-home.php
@@ -14,71 +14,88 @@
   <?php get_header();
   ?>
 
-  <section id="hero ">
+  <section id="hero">
     <!-- nu är heron 100vh men den ska vara 100vh minus headern höjd -->
-    <div class=" w-full h-screen bg-pink-200">
+    <div class=" bg-pink-100 ">
       <!-- ska vara en bild i denna div -->
-	    <?php if( get_field('home_hero') ): ?>
-            <img src="<?php the_field('home_hero'); ?>" />
-	    <?php endif; ?>
+      <img src="<?= get_template_directory_uri() ?>/assets/hero.jpg" alt="hero image" class="object-cover w-full vh-hero mt-[89px]">
       <div id="heroTitle" class=" bottom-0 absolute">
-        <h1>KINO KOSZYK</h1>
+        <h1 class="poppins text-[10vw] text-white ">KINO KOSZYK</h1>
       </div>
     </div>
 
   </section>
 
-  <section id="joanna">
-    <div class=" w-full h-screen bg-neutral-100 flex flex-row justify-center items-center">
-      <div id=joannaTextbox class="h-2/5 w-2/5 bg-yellow-50 ml-40">
-        <h3>Photographer</h3>
-        <h2>Joanna Helander</h2>
-        <p>Discover the beauty captured through the lens of our talented artist Joanna. Explore her stunning photography and allow yourself to be transported to new worlds of wonder and awe.</p>
-        <button class="btn-blue bottom-0">Button</button>
+  <section id="about">
+    <div class=" w-full vh-section bg-off-white flex flex-row justify-center items-center gap-[165px]">
+      <div id="eventPicture" class="h-[643.21px] w-[450px] bg-purple-300 ">
+        <img src="<?= get_template_directory_uri() ?>/assets/about.jpg" alt="about image" class="object-fill h-full w-full">
       </div>
+      <div id=eventTextbox class="w-[673px] h-[268px] gap-[32px] bg-purple-50">
+        <h2 class="font-prata text-[80px] leading-[86px] ">Kino Koszyk</h2>
+        <p class="poppins text-[20px] leading-[28px]">A text to raise the curiosity of visitors to look at Kino Kosyzk's previous events. Also a short text to inform visitors to look at Kino's Facebook page for more information about "smaller events".</p>
+        <button class="btn-blue mt-[50px]">Button</button>
+      </div>
+    </div>
+  </section>
 
-      <div id="pictureGrid" class="  m-auto grid grid-cols-2 grid-rows-2 h-5/6 w-2/6 bg-purple-300 gap-[30px]">
-        <div class="h-auto w-auto drop-shadow-lg bg-gray-50 ">
-          <p>1</p>
+  <section id="joanna">
+    <div class="w-full vh-section bg-white-red flex flex-row justify-center items-center gap-[165px]">
+      <div id=joannaTextbox class=" w-[673px] h-[331px] bg-yellow-50 gap-[32px] ">
+        <h3 class="poppins text-[26px] text-wine">Photographer</h3>
+        <h2 class="font-prata text-[80px] leading-[86px] ">Joanna Helander</h2>
+        <p class="poppins text-[20px] leading-[28px]">Discover the beauty captured through the lens of our talented artist Joanna. Explore her stunning photography and allow yourself to be transported to new worlds of wonder and awe.</p>
+        <button class="btn-blue mt-[50px]">Button</button>
+      </div>
+      <!-- tror de ville att picture grid skulle vara responsivt -->
+      <div id="pictureGrid" class=" grid grid-cols-2 grid-rows-2 h-[744.67px] w-[530px] gap-[30px]  ">
+        <div class=" h-[357.3px] w-[250px] drop-shadow-lg ">
+          <img src="<?= get_template_directory_uri() ?>/assets/hero.jpg" alt="hero image" class="object-fill h-full w-full">
         </div>
-        <div class="h-auto w-auto drop-shadow-lg bg-gray-50">
-          <p>2</p>
+        <div class="h-[357.3px] w-[250px] drop-shadow-lg bg-gray-50">
+          <img src="<?= get_template_directory_uri() ?>/assets/hero.jpg" alt="hero image" class="object-fill h-full w-full">
         </div>
-        <div class="h-auto w-auto drop-shadow-lg bg-gray-50">
-          <p>3</p>
+        <div class="h-[357.3px] w-[250px] drop-shadow-lg bg-gray-50">
+          <img src="<?= get_template_directory_uri() ?>/assets/hero.jpg" alt="hero image" class="object-fill h-full w-full">
         </div>
-        <div class="h-auto w-auto drop-shadow-lg bg-gray-50">
-          <p>4</p>
+        <div class="h-[357.3px] w-[250px] drop-shadow-lg bg-gray-50">
+          <img src="<?= get_template_directory_uri() ?>/assets/hero.jpg" alt="hero image" class="object-fill h-full w-full">
         </div>
       </div>
   </section>
 
   <section id="film">
-    <div class=" w-full h-screen bg-pink-300 flex flex-row justify-center items-center ">
-      <div id=filmTextbox class="h-2/5 w-3/6 bg-yellow-50 absolute right-0 mr-40">
-        <h2>Film</h2>
-        <p>Discover the beauty captured through the lens of our talented artist Joanna. Explore her stunning photography and allow yourself to be transported to new worlds of wonder and awe.</p>
-        <button class="btn-blue absolute bottom-0">Button</button>
+    <div class=" w-full vh-section bg-pink-300 flex justify-center items-center flex-col ">
+      <img src="<?= get_template_directory_uri() ?>/assets/film.jpg" alt="hero image" class="object-cover w-full vh-section">
+      <div id=filmTextbox class="w-[673px] h-[268px] absolute right-0 mr-[180px] gap-[32px] text-off-white bg-black">
+        <h2 class="font-prata text-[80px] leading-[86px] ">Film</h2>
+        <p class="poppins text-[20px] leading-[28px]">Discover the beauty captured through the lens of our talented artist Joanna. Explore her stunning photography and allow yourself to be transported to new worlds of wonder and awe.</p>
+        <button class="btn-blue mt-[50px]">Button</button>
       </div>
     </div>
   </section>
 
   <section id="event">
-    <div class=" w-full h-screen bg-blue-200 flex flex-row justify-center items-center">
-      <div id="eventPicture" class="h-4/6 w-1/4 bg-purple-300 absolute left-0 ml-40">
+    <div class=" w-full vh-section bg-white-red flex flex-row justify-center items-center gap-[165px]">
+      <div id="eventPicture" class="h-[643.21px] w-[450px] bg-purple-300 ">
         <p>1</p>
       </div>
-      <div id=eventTextbox class="h-2/5 w-3/6 bg-yellow-50 absolute right-0 mr-40">
-        <h2>Event</h2>
-        <p>A text to raise the curiosity of visitors to look at Kino Kosyzk's previous events. Also a short text to inform visitors to look at Kino's Facebook page for more information about "smaller events".</p>
-        <button class="btn-blue absolute bottom-0">Button</button>
-        <button class="btn-blue absolute bottom-0 right-0">Button</button>
+      <div id=eventTextbox class="w-[673px] h-[268px] mr-[180px] gap-[32px] bg-purple-50">
+        <h2 class="font-prata text-[80px] leading-[86px] ">Event</h2>
+        <p class="poppins text-[20px] leading-[28px]">A text to raise the curiosity of visitors to look at Kino Kosyzk's previous events. Also a short text to inform visitors to look at Kino's Facebook page for more information about "smaller events".</p>
+        <div id="btnContainer" class="flex flex-row space-x-20">
+          <button class="btn-blue absolute mt-[50px] ">Button</button>
+          <button class="btn-blue absolute mt-[50px]">Button</button>
+        </div>
       </div>
     </div>
   </section>
 
   <section id="partners">
 
+    <!-- <?php if (get_field('home_hero')) : ?>
+            <img src="<?php the_field('home_hero'); ?>" />
+	    <?php endif; ?> -->
   </section>
 
 
