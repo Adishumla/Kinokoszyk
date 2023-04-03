aebcce9c73bbc7162f47f5c25e22c496c8d58317
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-04-03 11:17:27 +0200

    added rick

--- wp-content/themes/kinokoszyk/page-joanna-helander.php
+++ wp-content/themes/kinokoszyk/page-joanna-helander.php
@@ -3,8 +3,8 @@
 </header>
 <section class="w-full h-2/3 flex flex-row justify-center items-center bg-white-red font-poppins text-off-black mt-[72px]">
 	<div class="ml-28 flex flex-col">
-		<h1 class="text-[80px] font-prata p-0 leading-[86px] max-w-[672px] align-center">Joanna Helander</h1>
-		<div class="w-2/6 min-w-[672px] mt-4">
+		<h1 class="text-[88px] font-prata p-0 leading-[86px] align-center">Joanna Helander</h1>
+		<div class="w-2/6 min-w-[600px] mt-4">
 			<p class="text-xl flex flex-col gap-4">
                 <span class="">Learn about Joanna Helander, a photographer, writer, and filmmaker born in Ruda Slaska in 1948. Arrested for protesting the invasion of Czechoslovakia, she emigrated to Sweden in 1971 and became an accomplished photographer, receiving the Swedish "Photographer of the Year" award in 1983.
                 </span>
@@ -13,6 +13,9 @@
                 </span>
 			</p>
 		</div>
+        <div>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="text-[24px] font-bold text-off-black hover:text-red-500">Watch the video</a>
+        </div>
 	</div>
 	<img class="h-full pr-4 object-fill justify-self-end" src="<?php echo get_template_directory_uri(); ?>/assets/photo-roll.svg" alt="">
 </section>
