a5e6770cd8786c16d48402633db46858f5e73611
lovbackan <simon@lovbacka.se>
2023-04-03 10:20:14 +0200

    fixed the textboxes in home

--- wp-content/themes/kinokoszyk/page-home.php
+++ wp-content/themes/kinokoszyk/page-home.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
+
 <body>
 
 
@@ -16,11 +17,11 @@
 
   <section id="about">
     <div class=" w-full vh-section bg-off-white flex flex-row justify-center items-center gap-[165px]">
-      <div id="aboutPicture" class="h-[643.21px] w-[450px] bg-purple-300 shadowHome">
+      <div id="aboutPicture" class="h-[643.21px] w-[450px] shadowHome">
         <img src="<?= get_template_directory_uri() ?>/assets/about.jpg" alt="about image" class="object-fill h-full w-full shadowHome">
       </div>
-      <div id=aboutTextbox class="w-[673px] h-[268px] gap-[32px] bg-purple-50">
-        <h2 class="font-prata text-[80px] leading-[86px]">Kino Koszyk</h2>
+      <div id=aboutTextbox class="w-[673px] h-[268px] ">
+        <h2 class="font-prata text-[80px] leading-[58px] mb-[26px]">Kino Koszyk</h2>
         <p class="poppins text-[20px] leading-[28px]">A text to raise the curiosity of visitors to look at Kino Kosyzk's previous events. Also a short text to inform visitors to look at Kino's Facebook page for more information about "smaller events".</p>
         <button class="btn-wine">
           <div class="arrow ml-[178.5px]"><img src="<?= get_template_directory_uri() ?>/assets/arrow-white.svg" alt="arrow right"></div>
@@ -34,9 +35,9 @@
 
   <section id="joanna">
     <div class="w-full vh-section bg-white-red flex flex-row justify-center items-center gap-[165px]">
-      <div id=joannaTextbox class=" w-[673px] h-[331px] bg-yellow-50 gap-[32px] ">
+      <div id=joannaTextbox class=" w-[673px] h-[331px] gap-[32px] ">
         <h3 class="poppins text-[26px] text-wine">Photographer</h3>
-        <h2 class="font-prata text-[80px] leading-[86px] ">Joanna Helander</h2>
+        <h2 class="font-prata text-[80px] leading-[86px] mb-[22px]">Joanna Helander</h2>
         <p class="poppins text-[20px] leading-[28px]">Discover the beauty captured through the lens of our talented artist Joanna. Explore her stunning photography and allow yourself to be transported to new worlds of wonder and awe.</p>
         <button class="btn-wine">
           <div class="arrow ml-[179px]"><img src="<?= get_template_directory_uri() ?>/assets/arrow-white.svg" alt="arrow right"></div>
@@ -65,8 +66,8 @@
   <section id="film">
     <div class=" w-full vh-section bg-pink-300 flex justify-center items-center flex-col ">
       <img src="<?= get_template_directory_uri() ?>/assets/film.jpg" alt="hero image" class="object-cover w-full vh-section">
-      <div id=filmTextbox class="w-[673px] h-[268px] absolute right-0 mr-[180px] gap-[32px] text-off-white bg-black">
-        <h2 class="font-prata text-[80px] leading-[86px] ">Film</h2>
+      <div id=filmTextbox class="w-[673px] h-[268px] absolute right-0 mr-[180px] gap-[32px] text-off-white">
+        <h2 class="font-prata text-[80px] leading-[58px] mb-[26px] ">Film</h2>
         <p class="poppins text-[20px] leading-[28px]">Discover the beauty captured through the lens of our talented artist Joanna. Explore her stunning photography and allow yourself to be transported to new worlds of wonder and awe.</p>
         <button class="btn-white">
           <div class="arrow ml-[170px]"><img src="<?= get_template_directory_uri() ?>/assets/arrow-black.svg" alt="arrow right"></div>
@@ -83,8 +84,8 @@
       <div id="eventPicture" class="h-[643.21px] w-[450px] bg-purple-300 shadowHome ">
         <img src="<?= get_template_directory_uri() ?>/assets/event-home.jpg" alt="event image" class="object-fill h-full w-full">
       </div>
-      <div id=eventTextbox class="w-[673px] h-[268px] mr-[180px] gap-[32px] bg-purple-50">
-        <h2 class="font-prata text-[80px] leading-[86px] ">Event</h2>
+      <div id=eventTextbox class="w-[673px] h-[268px] mr-[180px] gap-[32px]">
+        <h2 class="font-prata text-[80px] leading-[58px] mb-[26px] ">Event</h2>
         <p class="poppins text-[20px] leading-[28px]">A text to raise the curiosity of visitors to look at Kino Kosyzk's previous events. Also a short text to inform visitors to look at Kino's Facebook page for more information about "smaller events".</p>
         <div id="btnContainer" class="flex flex-row space-x-20">
           <button class="btn-white text-black">
@@ -106,6 +107,12 @@
   </section>
 
   <section id="partners">
+    <div class="w-full h-[383.7px] bg-pink-200">
+      <h3>
+        Här ska partners sektionen visas
+      </h3>
+
+    </div>
 
     <!-- <?php if (get_field('home_hero')) : ?>
             <img src="<?php the_field('home_hero'); ?>" />
