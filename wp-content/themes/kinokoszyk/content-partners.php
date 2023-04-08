e1c1452e257b955a3ab64ff53ca8c62a17658f33
DanFogelberg <dan.f.fogelberg@gmail.com>
2023-04-08 16:22:24 +0200

    Partners section desktop good enough

--- wp-content/themes/kinokoszyk/content-partners.php
+++ wp-content/themes/kinokoszyk/content-partners.php
@@ -10,8 +10,8 @@ $size = "full";
 ?>
 <div class="bg-off-white pb-24">
 
-    <h2 class="pt-[60px] font-prata text-[38px] leading-[40px] mb-[26px] text-center">Our Partners</h2>
-    <div class="box-border ml-[5%] mr-[5%] flex flex-wrap flex-none justify-center gap-x-[3vw] gap-y-[3vw]"> 
+    <h2 class="pt-[60px] font-poppins text-[38px] leading-[40px] font-bold mb-[36px] text-center">Our partners</h2>
+    <div class="box-border ml-[10%] mr-[10%] flex-wrap flex justify-center gap-x-[4vw] gap-y-[3vw]"> 
  
         
         <?
@@ -20,9 +20,9 @@ $size = "full";
     {
         if(empty($partner)) continue; 
         ?>
-        <div class="w-1/6 h-auto" >
+        <div class="w-[11vw] h-[3vw] flex object-contain " >
             
-            <img src=<?=$partner["url"]?> alt="<?=$partner["alt"]?>" class="max-h-[10vw]" srcset="<?= wp_get_attachment_image_srcset($partner["id"])?>" sizes="15vw">
+            <img class="object-contain w-full" src=<?=$partner["url"]?> alt="<?=$partner["alt"]?>"  srcset="<?= wp_get_attachment_image_srcset($partner["id"])?>" sizes="15vw">
         </div>
             
       
