56e9afb321ce2a41b8b5b5bb13043335822646a9
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-04-09 17:16:31 +0200

    p responsive

--- wp-content/themes/kinokoszyk/page-joanna-helander.php
+++ wp-content/themes/kinokoszyk/page-joanna-helander.php
@@ -4,9 +4,9 @@
 <section class="w-full h-4/4 flex flex-row justify-center items-center bg-white-red font-poppins text-off-black mt-[72px] mb-20">
 	<div class=" flex flex-col w-2/4">
 		<h1 class="text-[88px] font-prata p-0 leading-[86px] align-center">Joanna Helander</h1>
-		<div class="w-2/6 min-w-[600px] mt-4">
-			<p class="text-xl flex flex-col gap-4">
-                <span class="">Learn about Joanna Helander, a photographer, writer, and filmmaker born in Ruda Slaska in 1948. Arrested for protesting the invasion of Czechoslovakia, she emigrated to Sweden in 1971 and became an accomplished photographer, receiving the Swedish "Photographer of the Year" award in 1983.
+		<div class="w-2/6 max-w-[600px] mt-4 w-full mr-4">
+			<p class="text-xl flex flex-col gap-4 w-full">
+                <span class="w-full">Learn about Joanna Helander, a photographer, writer, and filmmaker born in Ruda Slaska in 1948. Arrested for protesting the invasion of Czechoslovakia, she emigrated to Sweden in 1971 and became an accomplished photographer, receiving the Swedish "Photographer of the Year" award in 1983.
                 </span>
 				<span>
 				Her early contacts with Poland's democratic opposition led her to direct a festival of independent Polish culture in Sweden in 1981, and she went on to publish books of photography and translations of Polish poetry. She has also directed and produced several films based on her own photographs. Joanna Helander's work has appeared in publications and television programs in Poland, Germany, and the U.S.A.
@@ -18,7 +18,9 @@
             <a href="#interview-section" id="interview-button" class="btn-wine py-2.5 px-5 text-center flex items-center justify-center m-0">Read her interview</a>
         </div>
 	</div>
+    <div>
 	<img class="h-full object-fill justify-self-end shadow-2xl" src="<?php echo get_template_directory_uri(); ?>/assets/Joanna-interview.jpg" alt="">
+    </div>
 </section>
 <?php
 $images_per_page = 15;
