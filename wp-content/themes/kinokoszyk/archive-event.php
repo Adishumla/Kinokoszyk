0bf62d127bc41cd26ffdaa5218aeb69a5e912f87
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-04-09 18:01:32 +0200

    added styling

--- wp-content/themes/kinokoszyk/archive-event.php
+++ wp-content/themes/kinokoszyk/archive-event.php
@@ -11,17 +11,18 @@ $events = new WP_Query( $args );
 get_header();
 ?>
 
-    <section class="w-full h-2/3 flex flex-row justify-center items-center bg-white-red poppins text-off-black mt-[72px] mb-[72px]">
-        <div class="ml-28">
-            <h1 class="text-3xl font-light text-wine">Exhibitions by Joanna Helander and Kino Koszyk</h1>
-            <h2 class="text-[148px] font-prata p-0 leading-[148px]">Events</h2>
-            <div class="w-3/6 min-w-[800px] mt-4">
-                <p class="text-xl font-poppins">Here you'll find information on upcoming shows and exhibitions featuring the innovative work of this Kino Koszyk and Joanna Helander. They are visionary filmmakers and authors whose work has been widely recognized and celebrated for its creativity and originality. Stay tuned for updates on upcoming events and be sure to join us for an unforgettable experience.</p>
-            </div>
+<section class="h-2/3 flex flex-row justify-center items-center bg-white-red poppins text-off-black mt-[72px] mb-[72px] ml-[72px]">
+    <div class="ml-28">
+        <h1 class="text-3xl font-light text-wine">Exhibitions by Joanna Helander and Kino Koszyk</h1>
+        <h2 class="text-[148px] font-prata p-0 leading-[148px]">Events</h2>
+        <div class="w-3/6 max-w-[800px] mt-4 w-full">
+            <p class="text-xl font-poppins">Here you'll find information on upcoming shows and exhibitions featuring the innovative work of this Kino Koszyk and Joanna Helander. They are visionary filmmakers and authors whose work has been widely recognized and celebrated for its creativity and originality. Stay tuned for updates on upcoming events and be sure to join us for an unforgettable experience.</p>
         </div>
-        <img class="h-full pr-0 object-fill justify-self-end" src="<?= get_template_directory_uri(); ?>/assets/events.svg" alt="">
-    </section>
-<section class="bg-off-white flex flex-row-reverse justify-between items-center p-20">
+    </div>
+    <img class="object-fill justify-self-end w-auto max-w-[580px] mr-0 pr-0 ml-36" src="<?= get_template_directory_uri(); ?>/assets/events.svg" alt="" style="width: auto !important;">
+</section>
+
+<section class="bg-off-white flex flex-row-reverse justify-between items-center py-12 px-44 w-full">
     <div>
         <a href="<?php echo get_permalink( get_page_by_path( 'photos' ) ); ?>"class="btn-wine py-2.5 px-5 text-center flex items-center justify-center">Events on Facebook <img class="ml-2 rotate-180" src="<?php echo get_template_directory_uri(); ?>/assets/arrow-white.svg" alt=""></a>
     </div>
@@ -34,7 +35,7 @@ get_header();
     </p>
     </div>
 </section>
-<section class="flex flex-col justify-center items-center bg-off-white py-20">
+<section class="flex flex-col justify-center items-center bg-off-white py-40">
 <?php if ( $events->have_posts() ) : ?>
     <?php while ( $events->have_posts() ) : $events->the_post(); ?>
         <?php
@@ -50,9 +51,9 @@ get_header();
         <div class="flex flex-row gap-10 text-[20px] mb-20 shadow-2xl w-3/4 mx-auto">
             <img class="w-96 h-auto object-cover aspect-square" src=<?= $image['url'] ?> alt="">
             <div class="flex flex-col justify-center items-start p-10">
-            <h2 class="text-3xl font-bold"><?= the_title() ?></h2>
-            <p class="text-2xl font-poppins pb-2">Year: <?=$year?> </p>
-        <p class="text-xl font-poppins"><?= the_content() ?></p>
+            <h2 class="text-4xl font-bold"><?= the_title() ?></h2>
+            <p class="text-2xl font-poppins pt-2">Year: <?=$year?> </p>
+        <p class="text-xl font-poppins pt-8"><?= the_content() ?></p>
             </div>
     </div>
         <?php endwhile;
@@ -60,7 +61,7 @@ get_header();
     wp_reset_query();
     // Pagination
 $big = 999999999; // need an unlikely integer
-echo '<div class="flex justify-center pb-10 align-center">';
+echo '<div class="flex justify-center align-center pt-10">';
 $arrow = get_template_directory_uri() . '/assets/Arrow.svg';
 $current_page = max( 1, get_query_var( 'paged' ) );
 $total_pages = $events->max_num_pages;
