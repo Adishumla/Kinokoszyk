2fa1ba0b6d66afd5c4691c8e57bc5bca3deb1320
lovbackan <simon@lovbacka.se>
2023-04-11 11:37:36 +0200

    mobile and ipad done on home

--- wp-content/themes/kinokoszyk/page-home.php
+++ wp-content/themes/kinokoszyk/page-home.php
@@ -1,25 +1,24 @@
 <?php get_header(); ?>
 
-<?php get_header(); ?>
-
 
 <section id="hero">
   <div>
-    <img src="<?= get_template_directory_uri() ?>/assets/compressedPictures/heroCompressed.webp" alt="Hero Image of a parade on a mountain" class="object-cover w-full vh-hero">
+    <img src="<?= get_template_directory_uri() ?>/assets/compressedPictures/heroCompressed.webp" alt="Hero Image of a parade on a mountain" class="object-cover w-full vh-hero-image">
     <div id="heroTitle" class=" bottom-0 absolute">
-      <h1 class="poppins sm:text-[100px] sm:leading-[76px] lg:text-[200px] lg:leading-[152px] text-white">KINO KOSZYK</h1>
+      <h1 class="poppins text-[80px] leading-[64px] sm:text-[100px] sm:leading-[76px] lg:text-[200px] lg:leading-[152px] text-white">KINO KOSZYK</h1>
     </div>
   </div>
 
 </section>
 <section id="about" class="px-8 bg-off-white ">
-  <div class=" w-full vh-section flex flex-row justify-evenly items-center gap-[5%]">
-    <div id="aboutPicture" class=" shadowHome sm:max-w-[243px] sm:max-h-[341px] h-7/12 w-5/12 lg:max-w-[450px] lg:max-h-[643.21px]">
-      <img src="<?= get_template_directory_uri() ?>/assets/compressedPictures/umbrellaMan.webp" alt="about image of a man with an umbrella staring over the ocean" class="object-cover">
+  <div class=" w-full h-[1164px] sm:h-[649px] lg:h-[1100px] flex flex-col items-center sm:flex-row sm:justify-evenly sm:items-center gap-[48px] sm:gap-[5%]">
+    <div id="aboutPicture" class="">
+      <!-- mt-[48px] sm:mt-0 shadowHome w-[380px] h-[542px] sm:max-w-[243px] sm:max-h-[341px] sm:h-7/12 sm:w-5/12 lg:max-w-[450px] lg:max-h-[643.21px] -->
+      <img src="<?= get_template_directory_uri() ?>/assets/compressedPictures/umbrellaMan.webp" alt="about image of a man with an umbrella staring over the ocean" class="object-cover shadowHome mt-[48px] w-[380px] h-[542px] lg:w-[450px] lg:h-[643.21px] sm:mt-0 sm:w-[243px] sm:h-[341px]">
     </div>
-    <div id=aboutTextbox class="textboxHome sm:w-[319px] lg:w-[673px]">
-      <h2 class="font-prata sm:text-[52px] sm:leading-[40px] lg:text-[80px] lg:leading-[58px] mb-[26px]">Kino Koszyk</h2>
-      <p class="poppins sm:text-[18px] sm:leading-[26px] lg:text-[20px] lg:leading-[28px]">With a passion for storytelling and a commitment to authenticity, Kino Koszyk produces thought-provoking documentaries that provide insight into the complex and fascinating history, culture, and people of Poland. Through their work, they aim to promote greater understanding and appreciation of this beautiful country and its people.</p>
+    <div id=aboutTextbox class="flex flex-col justify-center w-[380px] sm:w-[319px] lg:w-[673px]">
+      <h2 class="font-prata text-[52px] leading-[60px]  ls:text-[60px] ls:leading-[68px] lg:text-[80px] lg:leading-[86px] mb-[26px]">Kino Koszyk</h2>
+      <p class="poppins text-[18px] leading-[28px] sm:text-[20px] sm:leading-[28px]">With a passion for storytelling and a commitment to authenticity, Kino Koszyk produces thought-provoking documentaries that provide insight into the complex and fascinating history, culture, and people of Poland. Through their work, they aim to promote greater understanding and appreciation of this beautiful country and its people.</p>
       <a class="btn-wine flex items-center mt-[60px]" href="<?php echo get_permalink(get_page_by_path("about")) ?>">
         <div class="arrow ml-[178.5px]"><img src="<?= get_template_directory_uri() ?>/assets/arrow-white.svg" alt="arrow right"></div>
         <div class="ml-[69.5px]">
@@ -32,11 +31,12 @@
 
 
 <section id="joanna" class="px-8 bg-white-red">
-  <div class="w-full vh-section  flex flex-row justify-evenly items-center gap-[5%]">
-    <div id=joannaTextbox class=" textboxHome sm:w-[319px] lg:w-[673px]">
-      <h3 class="poppins sm:text-[24px] lg:text-[26px] text-wine">Photographer</h3>
-      <h2 class="font-prata sm:text-[52px] sm:leading-[40px] lg:text-[80px] lg:leading-[86px] mb-[26px]">Joanna Helander</h2>
-      <p class="poppins sm:text-[18px] sm:leading-[26px] lg:text-[20px] lg:leading-[28px]">Discover the beauty captured through the lens of our talented artist Joanna. Explore her stunning photography and allow yourself to be transported to new worlds of wonder and awe.</p>
+  <div class="w-full h-[1244px] sm:h-[649px] lg:h-[1100px] flex flex-col-reverse items-center sm:flex-row sm:justify-evenly sm:items-center sm:gap-[5%]">
+    <div id=joannaTextbox class=" flex flex-col mb-[100px] sm:mb-0 justify-center w-[380px] sm:w-[319px] lg:w-[673px]">
+      <h3 class="poppins text-[24px] leading-[28px] sm:text-[24px] sm:leading-[28px] lg:text-[26px] lg:leading-[37px] text-wine">Photographer</h3>
+      <!-- För att få Joanna Helander att lägga sig på rad så måste fonten vara en annan storlek samt leadingen. -->
+      <h2 class="font-prata text-[52px] leading-[60px]  ls:text-[60px] ls:leading-[68px] lg:text-[80px] lg:leading-[86px] mb-[26px]">Joanna Helander</h2>
+      <p class="poppins text-[18px] leading-[28px] sm:text-[20px] sm:leading-[28px]">Discover the beauty captured through the lens of our talented artist Joanna. Explore her stunning photography and allow yourself to be transported to new worlds of wonder and awe.</p>
       <a class="btn-wine flex items-center mt-[60px]" href="<?php echo get_permalink(get_page_by_path("photos")) ?>">
         <div class="arrow ml-[179px]"><img src="<?= get_template_directory_uri() ?>/assets/arrow-white.svg" alt="arrow right"></div>
         <div class="ml-[69px]">
@@ -45,28 +45,29 @@
       </a>
     </div>
 
-    <div id="pictureGrid" class=" grid grid-cols-2 grid-rows-2 sm:max-w-[243px] sm:max-h-[341px] sm:gap-[13.74px] lg:max-h-[744.67px] lg:max-w-[530px] lg:gap-[30px] w-6/12 h-8/12 ">
-      <div class=" max-h-[357.3px] max-w-[250px] shadowHome">
+    <div id="pictureGrid" class="grid grid-cols-2 grid-rows-2 w-[378px] h-[638px] gap-[21.4px] mb-[48px] lg:h-[744.67px] lg:w-[530px] lg:gap-[30px] sm:w-[243px] sm:h-[341px] sm:gap-[13.75px]">
+      <!-- w-[378px] h-[638px] gap-[25.7px] sm:max-w-[243px] sm:max-h-[341px] sm:gap-[13.74px] lg:max-h-[744.67px] lg:max-w-[530px] lg:gap-[30px] ls:w-6/12 ls:h-8/12 mb-[48px] sm:mb-0 -->
+      <div class=" shadowHome">
         <img src="<?= get_template_directory_uri() ?>/assets/compressedPictures/womanGroupSmall.webp" alt="Photograph example of ladies sitting together on the ground laughing" class="object-cover h-full w-full ">
       </div>
-      <div class="max-h-[357.3px] max-w-[250px] shadowHome ">
+      <div class=" shadowHome ">
         <img src="<?= get_template_directory_uri() ?>/assets/compressedPictures/womanWorkingSmall.webp" alt="Photograph example of a woman working" class="object-fill h-full w-full">
       </div>
-      <div class="max-h-[357.3px] max-w-[250px] shadowHome ">
+      <div class=" shadowHome ">
         <img src="<?= get_template_directory_uri() ?>/assets/compressedPictures/womanSmokingSmall.webp" alt="Photograph example of a woman smoking" class="object-cover h-full w-full">
       </div>
-      <div class="max-h-[357.3px] max-w-[250px] shadowHome">
+      <div class=" shadowHome">
         <img src="<?= get_template_directory_uri() ?>/assets/compressedPictures/powerSmall.webp" alt="Phtograph example of the womensday in ruda 1976" class="object-fill h-full w-full">
       </div>
     </div>
 </section>
 
 <section id="film">
-  <div class=" w-full vh-section  flex justify-center items-center flex-col ">
-    <img src="<?= get_template_directory_uri() ?>/assets/compressedPictures/filmCompressed.webp" alt="Big photograph of a woman sitting on stairs of a ruined building" class="object-cover w-full vh-section">
-    <div id=filmTextbox class="lg:w-[673px] sm:w-[319px] lg:h-[268px] absolute right-0 lg:mr-[180px] sm:mr-[88px] gap-[32px] text-off-white">
-      <h2 class="font-prata sm:text-[52px] sm:leading-[40px] lg:text-[80px] lg:leading-[58px] mb-[26px] ">Film</h2>
-      <p class="poppins sm:text-[18px] sm:leading-[26px] lg:text-[20px] lg:leading-[28px]">Experience the magic of cinema with Kino Kozsyks. Our films take you on a journey beyond the screen, with captivating stories and stunning visuals that will leave you inspired. Don't miss out on the opportunity to immerse yourself in the world of Kino Kozsyks. </p>
+  <div class=" w-full h-[1106px] sm:h-[649px] lg:h-[1100px] flex justify-center items-center flex-col ">
+    <img src="<?= get_template_directory_uri() ?>/assets/compressedPictures/filmCompressed.webp" alt="Big photograph of a woman sitting on stairs of a ruined building" class="object-cover w-full h-[1106px] sm:h-[649px] lg:h-[1100px]">
+    <div id=filmTextbox class="w-[380px] h-[370px] lg:w-[673px] sm:w-[319px] lg:h-[268px] absolute mt-[636px] sm:mt-0 sm:right-0 lg:mr-[180px] sm:mr-[88px] gap-[32px] text-off-white">
+      <h2 class="font-prata text-[52px] leading-[60px]  ls:text-[60px] ls:leading-[68px] lg:text-[80px] lg:leading-[86px] mb-[26px]">Film</h2>
+      <p class="poppins text-[18px] leading-[28px] sm:text-[20px] sm:leading-[28px]">Experience the magic of cinema with Kino Kozsyks. Our films take you on a journey beyond the screen, with captivating stories and stunning visuals that will leave you inspired. Don't miss out on the opportunity to immerse yourself in the world of Kino Kozsyks. </p>
       <a class="btn-white flex items-center mt-[50px]" href="<?php echo get_permalink(get_page_by_path("films")) ?>">
         <div class="arrow ml-[170px]"><img src="<?= get_template_directory_uri() ?>/assets/arrow-black.svg" alt="arrow right"></div>
         <div class="ml-[78px] ">
@@ -78,13 +79,13 @@
 </section>
 
 <section id="event" class="px-8 bg-white-red">
-  <div class=" w-full vh-section flex flex-row items-center justify-evenly gap-[5%]">
-    <div id="eventPicture" class="shadowHome sm:max-w-[243px] sm:max-h-[341px] h-7/12 w-5/12 lg:max-w-[450px] lg:max-h-[643.21px]">
+  <div class=" w-full h-[1210px] sm:h-[649px] lg:h-[1100px] flex flex-col sm:flex-row items-center sm:justify-evenly gap-[48px] sm:gap-[5%]">
+    <div id="eventPicture" class="object-cover shadowHome mt-[48px] w-[380px] h-[542px] lg:w-[450px] lg:h-[643.21px] sm:mt-0 sm:w-[243px] sm:h-[341px]">
       <img src="<?= get_template_directory_uri() ?>/assets/compressedPictures/womanWorkingBig.webp" alt="Event image of a woman working" class="object-cover">
     </div>
-    <div id=eventTextbox class="textboxHome sm:w-[319px] lg:w-[673px]">
-      <h2 class="font-prata sm:text-[52px] sm:leading-[40px] lg:text-[80px] lg:leading-[58px] mb-[26px] ">Event</h2>
-      <p class="poppins sm:text-[18px] sm:leading-[26px] lg:text-[20px] lg:leading-[28px]">Discover the latest movie screenings and events at Kino Koszyk's event page! Immerse yourself in a world of cinema and entertainment with our diverse range of films, from classics to new releases. Join us for a unique cinematic experience in the heart of the city. Book your tickets now and be a part of the excitement!</p>
+    <div id=eventTextbox class="flex flex-col justify-center w-[380px] sm:w-[319px] lg:w-[673px]">
+      <h2 class="font-prata text-[52px] leading-[60px]  ls:text-[60px] ls:leading-[68px] lg:text-[80px] lg:leading-[86px] mb-[26px]">Event</h2>
+      <p class="poppins text-[18px] leading-[28px] sm:text-[20px] sm:leading-[28px]">Discover the latest movie screenings and events at Kino Koszyk's event page! Immerse yourself in a world of cinema and entertainment with our diverse range of films, from classics to new releases. Join us for a unique cinematic experience in the heart of the city. Book your tickets now and be a part of the excitement!</p>
       <div id="btnContainer" class="flex flex-row flex-wrap gap-[24px] mt-[50px]">
         <a class="btn-white flex items-center" href="<?php echo get_permalink(get_page_by_path("events")) ?>">
           <div class="arrow ml-[177.5px]"><img src="<?= get_template_directory_uri() ?>/assets/arrow-black.svg" alt="arrow right"></div>
