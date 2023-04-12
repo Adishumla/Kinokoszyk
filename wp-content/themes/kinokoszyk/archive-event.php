60e35295a75b7ea4904e33f295a56370b8cc1555
lovbackan <simon@lovbacka.se>
2023-04-12 23:00:46 +0200

    Fixed margins and stuff on event

--- wp-content/themes/kinokoszyk/archive-event.php
+++ wp-content/themes/kinokoszyk/archive-event.php
@@ -12,30 +12,33 @@ get_header();
 ?>
 
 <section class="mt-24">
-    <div class="grid grid-cols-3 gap-y-2 gap-x-4 py-6 pl-6 overflow-x-hidden sm:gap-x-14 md:pl-20 lg:gap-y-0 lg:pl-44 lg:pt-28 lg:pb-20">
+    <!-- <div class="grid grid-cols-3 gap-y-2 gap-x-4 py-6 pl-6 sm:pl-[88px] sm:py-[108px] overflow-x-hidden sm:gap-x-14 lg:gap-y-0 lg:pl-44 lg:pt-28 lg:pb-20"> -->
+
+
+    <div class="grid grid-cols-3 gap-y-2 gap-x-4 py-6 pl-6 overflow-x-hidden sm:gap-x-14 sm:pl-20 lg:gap-y-0 lg:pl-44 lg:pt-28 lg:pb-20">
         <h1 class="col-span-2 col-start-1 text-2xl font-light text-wine lg:text-3xl">Exhibitions by Joanna Helander and Kino Koszyk</h1>
         <h2 class="col-span-2 col-start-1 text-5xl mb-4 font-prata p-0 sm:text-6xl lg:text-[148px]  lg:leading-[148px]">Events</h2>
-        <img class=" row-start-2 sm:row-start-1 col-start-3 row-span-2 object-fill" src="<?= get_template_directory_uri(); ?>/assets/events.svg" alt="">
+        <img class="  row-start-2 justify-self-end h-28 sm:h-60 sm:row-start-1 col-start-3 row-span-3 object-fill" src="<?= get_template_directory_uri(); ?>/assets/events.svg" alt="">
         <p class="col-span-3 col-start-1 text-lg font-poppins sm:col-span-2 sm:text-xl">Here you'll find information on upcoming shows and exhibitions featuring the innovative work of this Kino Koszyk and Joanna Helander. They are visionary filmmakers and authors whose work has been widely recognized and celebrated for its creativity and originality. Stay tuned for updates on upcoming events and be sure to join us for an unforgettable experience.</p>
     </div>
 </section>
 
-<section class="bg-off-white flex flex-col px-[20px] lg:flex-row lg:justify-between lg:items-center py-12 lg:px-44 w-full pb-[48px]">
-    <div class="ml-[] sm:ml-[88px] lg:ml-0">
-        <h4 class="text-[28px] sm:text-[34px] sm:leading-[] lg:text-[38px] sm:mb-[20px] font-bold lg:pb-2">
+<section class="bg-off-white flex flex-col lg:flex-row lg:justify-between lg:items-center py-12 lg:px-44 w-full lg:gap-[3%]">
+    <div class="px-[24px] sm:px-[80px] lg:px-0">
+        <h4 class="text-[28px] sm:text-[34px] lg:text-[38px] sm:mb-[20px] font-bold lg:pb-2">
             Looking for our latest events and exhibitions?
         </h4>
-        <p class="text-size[18px] sm:text-xl font-poppins max-w-[600px] sm:mb-[48px] mb-[32px]">
+        <p class="text-size[18px] sm:text-xl font-poppins max-w-[600px] sm:mb-[48px] mb-[32px] lg:mb-0">
             Look no further than our Facebook page! We post all the latest information and updates so you'll never miss a thing.
         </p>
     </div>
-    <div class="sm:ml-[88px] lg:ml-0 ">
+    <div class=" px-[24px] sm:px-[80px] lg:px-0 ">
         <a href="<?php echo get_permalink(get_page_by_path('photos')); ?>" class="btn-wine py-2.5 px-5 text-center flex items-center justify-center">Events on Facebook <img class="ml-2 rotate-180" src="<?php echo get_template_directory_uri(); ?>/assets/arrow-white.svg" alt=""></a>
     </div>
 </section>
 
 
-<section class="pt-[48px] sm:pt-0 lg:pt-0 grid grid-cols-1 sm:gap-[10px] sm:grid sm:grid-cols-2  lg:flex lg:flex-col lg:justify-center lg:items-center bg-off-white lg:py-40">
+<section class="pt-[48px] sm:pt-0 grid grid-cols-1 sm:gap-[10px] sm:grid sm:grid-cols-2  lg:flex lg:flex-col lg:justify-center lg:items-center bg-off-white lg:py-40 lg:gap-0 sm:pb-[80px] py-[48px]">
     <?php if ($events->have_posts()) : ?>
         <?php while ($events->have_posts()) : $events->the_post(); ?>
             <?php
@@ -53,8 +56,8 @@ get_header();
                 <div class="flex flex-col justify-center items-start p-10 ">
                     <h2 class="text-4xl font-bold"><?= the_title() ?></h2>
                     <p class="text-2xl font-poppins pt-2">Year: <?= $year ?> </p>
-                    <!-- fixa detta med break-words -->
-                    <p class="text-xl font-poppins pt-8 max-w-[260px] break-words "><?= the_content() ?></p>
+                    <!-- just nu kan det bli en bugg när man skriver ett väldigt lång ord men detta går att fixa med  break-words men man behöver en satt width då vilket blev jobbigt -->
+                    <p class="text-xl font-poppins pt-8 "><?= the_content() ?></p>
                 </div>
             </div>
     <?php endwhile;
@@ -62,12 +65,12 @@ get_header();
     wp_reset_query();
     // Pagination
     $big = 999999999; // need an unlikely integer
-    echo '<div class="flex justify-center align-center pt-10">';
+    echo '<div class="flex justify-center align-center lg:pt-10">';
     $arrow = get_template_directory_uri() . '/assets/Arrow.svg';
     $current_page = max(1, get_query_var('paged'));
     $total_pages = $events->max_num_pages;
     if ($total_pages > 1) {
-        echo '<div class="flex justify-center mb-10">';
+        echo '<div class="flex justify-center">';
         echo '<span class="screen-reader-text">' . __('Page', 'textdomain') . ' ' . $current_page . ' ' . __('of', 'textdomain') . ' ' . $total_pages . '</span>';
         echo '<a class="prev page-numbers" href="' . get_pagenum_link($current_page - 1) . '"><img src="' . $arrow . '" alt="arrow" class="transform inline-block"></a>';
         echo '<span class="self-center text-xl mx-5">' . __('Page ', 'textdomain') . $current_page . __(' of ', 'textdomain') . $total_pages . '</span>';
--- wp-content/themes/kinokoszyk/page-home.php
+++ wp-content/themes/kinokoszyk/page-home.php
@@ -65,7 +65,7 @@
 <section id="film">
   <div class=" w-full h-[1106px] sm:h-[649px] lg:h-[1100px] flex justify-center items-center flex-col ">
     <img src="<?= get_template_directory_uri() ?>/assets/compressedPictures/filmCompressed.webp" alt="Big photograph of a woman sitting on stairs of a ruined building" class="object-cover w-full h-[1106px] sm:h-[649px] lg:h-[1100px] object-left-top">
-    <div id=filmTextbox class="w-[380px] h-[370px] lg:w-[673px] sm:w-[319px] lg:h-[268px] absolute mt-[636px] sm:mt-0 sm:right-0 lg:mr-[180px] sm:mr-[88px] gap-[32px] text-off-white">
+    <div id=filmTextbox class="w-[380px] h-[370px] lg:w-[673px] sm:w-[319px] lg:h-[268px] absolute mt-[636px] sm:mt-0 sm:right-0 lg:mr-[180px] sm:mr-[15%] gap-[32px] text-off-white">
       <h2 class="font-prata text-[52px] leading-[60px]  ls:text-[60px] ls:leading-[68px] lg:text-[80px] lg:leading-[86px] mb-[26px]">Film</h2>
       <p class="poppins text-[18px] leading-[28px] sm:text-[20px] sm:leading-[28px]">Experience the magic of cinema with Kino Kozsyks. Our films take you on a journey beyond the screen, with captivating stories and stunning visuals that will leave you inspired. Don't miss out on the opportunity to immerse yourself in the world of Kino Kozsyks. </p>
       <a class="btn-white flex items-center mt-[50px]" href="<?php echo get_permalink(get_page_by_path("films")) ?>">
