f61921c7b35065af134a015a93d7ea61c518508b
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-04-13 23:05:31 +0200

    fixed alt text with full screen text

--- wp-content/themes/kinokoszyk/archive-photo.php
+++ wp-content/themes/kinokoszyk/archive-photo.php
@@ -69,24 +69,28 @@ $third = [];
 				foreach ($images as $image) {
 					preg_match('/src="([^"]+)"/i', $image, $image_src);
 					$image_url = $image_src[1];
-					$image_urls[] = $image_url;
+					$title = get_the_title();
+					$image_urls[] = array('url' => $image_url, 'title' => $title);
 				}
 			}
-			foreach ($image_urls as $image_url) { ?>
-    <div class="inline-block align-top overflow-hidden masonry-item w-full w-[38.9%] md:w-[26.3%] lg:w-[26.7%] mb-4">
-        <?php if (!empty($image_url) && filter_var($image_url, FILTER_VALIDATE_URL)) { ?>
-            <a href="<?php echo $image_url; ?>" target="_blank">
-                <img class="w-full hover:scale-110 transition duration-500 ease-in-out h-auto object-cover object-center" src="<?php echo $image_url; ?>" data-post-name="My Image Post Name" alt="">
-            </a>
-        <?php } else { ?>
-            <div class="w-full h-48 bg-gray-100 flex items-center justify-center">
-                <span class="text-gray-400">Image not available</span>
-            </div>
-        <?php } ?>
-    </div>
-<?php } ?>
+			?>
 
-		</div>
+			<?php foreach ($image_urls as $image) { ?>
+                <div class="inline-block align-top overflow-hidden masonry-item w-full w-[39.5%] md:w-[26.3%] lg:w-[26.7%] mb-4">
+					<?php if (!empty($image['url']) && filter_var($image['url'], FILTER_VALIDATE_URL)) { ?>
+                        <a href="<?php echo $image['url']; ?>" target="_blank">
+                            <img class="w-full hover:scale-110 transition duration-500 ease-in-out h-auto object-cover object-center" src="<?php echo $image['url']; ?>" alt="<?php echo $image['title']; ?>">
+                        </a>
+					<?php } else { ?>
+                        <div class="w-full h-48 bg-gray-100 flex items-center justify-center">
+                            <span class="text-gray-400">Image not available</span>
+                        </div>
+					<?php } ?>
+                </div>
+			<?php } ?>
+
+
+        </div>
 	</div>
 	<?php
 	wp_reset_postdata();
